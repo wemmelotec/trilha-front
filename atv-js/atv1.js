c22d790b2ed809a41bb7e87613930b0633b0211c
Professor <professor@ACADEMICOS.COM>
2025-08-09 22:01:40 -0300

    done

---
+++ atv-js/atv1.js
@@ -0,0 +1,10 @@
+// somar
+let a = 5;
+let b = 10;
+
+console.log("O Resultado da soma "+a+" + "+b+ " = " + (a+b));
+
+//subtrair
+let c = 5;
+let d = 10;
+console.log("O Resultado da subtração "+d+" - "+c+ " = " + (d-c));
